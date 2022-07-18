7cfca54ecd7b5b118789ddc742eda0aa3861e7f2
AliFarzi <94639001+AliFarzi@users.noreply.github.com>
2022-07-18 14:05:19 +0200

    Update run.sh
    Fixing bug

--- docker/run.sh
+++ docker/run.sh
@@ -10,7 +10,6 @@ sudo docker run -it \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v /tmp/argus_socket:/tmp/argus_socket \
     -v /etc/enctune.conf:/etc/enctune.conf \
-    -v $HOME/ali:/jetson-inference/ali \
     --device /dev/video0 \
     --device /dev/video1 \
     snapcon-workshop:heidelberg
