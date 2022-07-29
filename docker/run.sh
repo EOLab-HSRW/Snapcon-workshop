2838932ed0252e215f1959f85cab9646b585367b
jetson <Ali.Farzizada@hsrw.org>
2022-07-29 18:15:42 +0200

    Modified Detection.py

--- docker/run.sh
+++ docker/run.sh
@@ -3,13 +3,15 @@
 sudo xhost +si:localuser:root
 
 sudo docker run -it \
-    --runtime nvidia \
     --rm \
+    --runtime nvidia \
     --security-opt  seccomp=unconfined \
     --network host \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v /tmp/argus_socket:/tmp/argus_socket \
     -v /etc/enctune.conf:/etc/enctune.conf \
+    -v $PWD/python:/jetson-inference/snapcon/python \
+    -v $PWD/snap:/jetson-inference/snapcon/snap \
     --device /dev/video0 \
     --device /dev/video1 \
     snapcon-workshop:heidelberg
--- python/Detection.py
+++ python/Detection.py
@@ -13,27 +13,53 @@ import jetson.utils
 #initializing network
 connector = decNet.DetectNetConnector()
 net = jetson.inference.detectNet("ssd-mobilenet-v2")
+#Assigning csi camera as input Source
+camera = jetson.utils.videoSource("csi://0")  #"/dev/video0"
 
 
+def reSizeImage():
+	# Capture image from jetson camera (cuda Image)
+	image =  camera.Capture()
+            
+        #Resizing the image for Snap (1270*720) --> (480*360)
+	reSizedImage = jetson.utils.cudaAllocMapped(width = image.width * 0.375, height = image.height * 0.5, format = image.format)
+	jetson.utils.cudaResize(image, reSizedImage)
+
+	return reSizedImage
 
 async def process(websocket,path):
-
+	sendImage = False
 	while not websocket.closed:
-
+		sendFrame = False
 		async for message in websocket:
-			# splitting mime-type from base64
-			_, img_encoded = message.split(',')
-
-			img_decoded = base64.b64decode(img_encoded)
-			file_name = 'myImage.jpg'
-
-
-			with open (file_name , 'wb') as f:
-				f.write(img_decoded)
-
-			# loading image to CPU
-			image = jetson.utils.loadImage('myImage.jpg')
 			
+			# deciding whether to send rendered image or not  
+			if message == "sendImage":
+				sendImage = True
+				break
+			if message == "notSendImage":
+				sendImage = False					
+				break
+			# ask for image from jetson camera
+			if message == "frame":
+				sendFrame = True
+				image = reSizeImage()
+			else:
+				# splitting mime-type from base64
+				_, img_encoded = message.split(',')
+
+				img_decoded = base64.b64decode(img_encoded)
+				file_name = 'myImage.jpg'
+
+
+				with open (file_name , 'wb') as f:
+					f.write(img_decoded)
+
+				# loading image to CPU
+				image = jetson.utils.loadImage('myImage.jpg')
+			
+
+
 			# running inference
 			detections = connector.RunInference(image,net)
 
@@ -48,7 +74,12 @@ async def process(websocket,path):
 			jpg_as_text = base64.b64encode(buffer)
 			
 			# sending detections list and image back to client
-			response = {"detections":detections, "image":str(jpg_as_text.decode('utf-8'))}
+			if sendImage or sendFrame:
+			
+				response = {"detections":detections, "image":str(jpg_as_text.decode('utf-8'))}
+			else:
+				response = {"detections":detections}
+			
 			response = json.dumps(response)
 			
 
